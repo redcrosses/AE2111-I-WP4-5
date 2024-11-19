2e37b96974cc331597f37f594d46ed2a4956001c
redcrosses <94404359+redcrosses@users.noreply.github.com>
2024-11-19 17:31:19 +0100

    wingbox drawing relative to airfoil (unfinished)

--- WP4.2/main2.py
+++ WP4.2/main2.py
@@ -2,24 +2,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy as sp
 from scipy.integrate import quad
-
-
+import points_intersection
 #Constraints
 #- The wing tip displacement should not exceed 15% of the total span of the wing.
 #- The wing tip rotation should not exceed +/- 10°.
 
 class WingBox():
-    def __init__(self, frontsparpos, rearsparpos, length):
+    def __init__(self, frontsparpos, rearsparpos):
         self.frontsparpos: float = frontsparpos
         self.rearsparpos: float = rearsparpos
-        self.length: float = length
+        self.length: float = self.rearsparpos - self.frontsparpos
         
     def draw(self):
-        f = open("WP4.2/fx60126.dat","r")
-        print(f.read())
-        self.coords = [[self.frontsparpos,0],[self.frontsparpos, 1],[self.rearsparpos, 1], [self.rearsparpos, 0.5],[self.frontsparpos, 0]]
-        x,y = zip(*self.coords)
-        plt.plot(x,y)
+        #airfoil
+        self.x1 = np.array([0])
+        self.y1 = np.array([0])
+        data = np.loadtxt("WP4.2/fx60126.dat")
+        self.x1 = data[:,0]
+        self.y1 = data[:,1]
+        
+        #wingbox
+        self.sparpos = points_intersection.run([self.frontsparpos, self.rearsparpos])
+        # print(self.sparpos)
+        self.x2,self.y2 = zip(*self.sparpos)
+        
+        plt.plot(self.x1,self.y1)
+        plt.plot(self.x2,self.y2)
+        plt.gca().set_aspect('equal')
         plt.show()
     def centroid(self):
         pass
@@ -28,23 +37,8 @@ class WingBox():
 #draw/have the geomerty of a wing box
 #calculate the centroid, second moment of area
 
-box = WingBox(2,3,1)
+box = WingBox(0.1,0.5) #box from 10%c to 50%c
 box.draw()
-
-
-
-def MOI_x(wingbox: list[tuple], stringer_area: float, y: float, thickness: float, chord) -> float:
-    centroid_x: float = find.centroid()
-
-
-
-
-
-
-
-    return
-def G(y: float) -> float:
-    return
 
 def bending_displacement(MOI: float, loads: list) -> list[float]:
     return

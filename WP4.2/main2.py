015fdaeb88ad9581084fc6479976f214531a3685
redcrosses <94404359+redcrosses@users.noreply.github.com>
2024-11-21 14:19:23 +0100

    fix

--- WP4.2/main2.py
+++ WP4.2/main2.py
@@ -42,7 +42,7 @@ box.draw()
 #box.trapezoid provides the trapezoid points, 
 #box.frontsparlength provides the front spar length
 #box.rearsparlength provides the rear spar length
-#box.length provides the length between the spars
+#box.width provides the length between the spars
 
 #calculate the centroid, second moment of area
 
@@ -54,22 +54,15 @@ def MOI_x(wingbox: list[tuple], stringer_area: float, stringer_positions: list[t
     b = box.length/np.cos(theta)
 
     I_xx_1 = thickness * (a**3)*(np.sin(beta)**2) * (1/12) + (thickness*a) * ((a/2)*np.sin(beta)+(wingbox[0][1]-centroid[1]))**2
-    I_xx_2 = (box.frontsparheight * thickness**3)*(1/12) + (thickness*box.frontsparheight) * (((wingbox[0][1]+wingbox[4][1])/2)-centroid[1])**2
+    I_xx_2 = (box.frontsparlength * thickness**3)*(1/12) + (thickness*box.frontsparlength) * (((wingbox[0][1]+wingbox[4][1])/2)-centroid[1])**2
     I_xx_3 = thickness * (b**3)*(np.sin(theta)**2) * (1/12) + (thickness*b) * ((b/2)*np.sin(theta)+(wingbox[3][1]-centroid[1]))**2
-    I_xx_4 = (box.rearsparheight * thickness**3)*(1/12) + (thickness*box.rearsparheight) * (((wingbox[2][1]+wingbox[3][1])/2)-centroid[1])**2
+    I_xx_4 = (box.rearsparlength * thickness**3)*(1/12) + (thickness*box.rearsparlength) * (((wingbox[2][1]+wingbox[3][1])/2)-centroid[1])**2
 
     I_stringers = sum([stringer_area*(abs(pos[1])-centroid[1])**2 for pos in stringer_positions])
     return I_xx_1 + I_xx_2 + I_xx_3 + I_xx_4 + I_stringers
 
 def J(y: float) -> float:
     return
-
-def bending_displacement(MOI: float, loads: list) -> list[float]:
-    return
-
-def torsion_rotation(G: float, loads: list) -> list[float]:
-    return
-
 
 #compute deflection profiles of the wing (app.D.2)
 
@@ -89,7 +82,9 @@ def GJ(y):
     #shiyu function * G
     return y
 
-def dv_dy(y):
+#deflection functions
+
+def dv_dy(y): 
     integral, _ = quad(lambda x: -Mx(x) / EI_xx(x), 0, y)
     return integral
 

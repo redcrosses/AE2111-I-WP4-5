a1d1adf91f00a01866e8bbee08aa4b656e6ea9e0
redcrosses <94404359+redcrosses@users.noreply.github.com>
2024-12-12 16:01:55 +0100

    merge conflict fix

--- WP5_1/main4.py
+++ WP5_1/main4.py
@@ -1,9 +1,20 @@
-def K_c(a,b): # curve fit for skin buckling coefficient Kc
-    r=a/b
-    if r >0.69 and r<=1.12:
-        return -104.20542*r**4+357.54147*r**3-411.62491*r**2+165.89543*r
-    elif r<=1.81:
-        return -8.25872*r**4+45.66709*r**3-86.31837*r**2+60.2625*r
-    elif r<=5:
-        return 0.049532*r**4-0.782365*r**3+4.59504*r**2-11.99811*r+-11.99811
-    return None
+import numpy as np
+def main4(I_xx, trapezoids, span_and_chord, loads, spanwise_position):
+    def K_c(a,b): # curve fit for skin buckling coefficient Kc
+        r=a/b
+        if r >0.69 and r<=1.12:
+            return -104.20542*r**4+357.54147*r**3-411.62491*r**2+165.89543*r
+        elif r<=1.81:
+            return -8.25872*r**4+45.66709*r**3-86.31837*r**2+60.2625*r
+        elif r<=5:
+            return 0.049532*r**4-0.782365*r**3+4.59504*r**2-11.99811*r+-11.99811
+        return None
+    def Mx(y): 
+        return np.interp(y, spanwise_position, loads[0][1], 0)
+    def T(y): 
+        return np.interp(y, spanwise_position, loads[0][2], 0)
+    
+
+
+if __name__ == "__main__":
+    pass

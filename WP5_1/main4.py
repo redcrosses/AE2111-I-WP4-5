c62dbb73939f4c80606adae519a7bf2dbd6cfdf1
LennartH21 <lennart.hubbers@gmail.com>
2024-12-12 15:56:25 +0100

    Update main4.py

--- WP5_1/main4.py
+++ WP5_1/main4.py
@@ -6,3 +6,4 @@ def K_c(a,b): # curve fit for skin buckling coefficient Kc
         return -8.25872*r**4+45.66709*r**3-86.31837*r**2+60.2625*r
     elif r<=5:
         return 0.049532*r**4-0.782365*r**3+4.59504*r**2-11.99811*r+-11.99811
+    return None

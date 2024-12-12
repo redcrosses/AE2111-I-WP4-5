b8c79ca8e8af63aed737035345afd842b2277359
MacPos35 <macpos35@gmail.com>
2024-12-12 16:14:20 +0100

    SIGMA BOI

--- WP5_1/main4.py
+++ WP5_1/main4.py
@@ -17,8 +17,15 @@ def main4(I_xx, trapezoids, span_and_chord, loads, spanwise_position):
         r = a/b
         return 136.31117 - 378.14535*r + 497.60785*r**2 - 366.68125*r**3 + 163.8237*r**4 - 45.33579*r**5 + 7.595018*r**6  - 0.7056433*r**7 + 0.02790314*r**8
 
+    def tau_cr(ks, E, nu, t, b):
+        tau_cr = (np.pi**2 * ks * E) / (12 * (1 - nu**2)) * (t / b)**2
+        return tau_cr
+    
+    def sigma_cr(kc, E, nu, t, b):
+        sigma_cr = (np.pi**2 * kc * E) / (12 * (1 - nu**2)) * (t / b)**2
+        return sigma_cr
+
+
 
 if __name__ == "__main__":
     pass
-
-#Curve fit: Buckling coefficient for rectangular isotropic plates under shear

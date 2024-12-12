0d5064c8554fa2bc01fab36ef7f71f764b4a430d
CShiyyy <136488761+CShiyyy@users.noreply.github.com>
2024-12-12 15:33:01 +0100

    main 2 outputs

--- ACTUALLY_MAIN.py
+++ ACTUALLY_MAIN.py
@@ -1,6 +1,7 @@
 from WP4_1.main1 import main1
 from WP4_2.main2 import main2
 from WP4_3.main3 import main3
+from WP5_2.main5 import main5
 import sys
 
 if not sys.warnoptions:
@@ -16,7 +17,8 @@ print(n_negative)
 print("Finding planform loading diagrams...")
 loads_positive, loads_negative, spanwise_position = main1(n_positive, n_negative)
 # print(loads_positive, loads_negative)
-moi_x_list, trapezoids = main2((loads_positive, loads_negative), spanwise_position, (n_positive, n_negative), 0.1056927, 0.07702, 0.005, 0.01,  42, 2e-4)
+moi_x_list, trapezoid, stringer_positions, span_positions_and_chord = main2((loads_positive, loads_negative), spanwise_position, (n_positive, n_negative), 0.1056927, 0.07702, 0.005, 0.01,  42, 2e-4)
 # main2((loads_positive, loads_negative), spanwise_position, (n_positive, n_negative), 0.12079, 0.07702, 0.01, 0.01, 42, 4e-4)
 # main2((loads_positive, loads_negative), spanwise_position, (n_positive, n_negative), 0.12079, 0.06808, 0.01, 0.015, 42, 4e-4)
 
+# main5(moi_x_list, trapezoids, span_positions, (loads_positive, loads_negative))
--- WP5_2/main5.py
+++ WP5_2/main5.py
@@ -1,4 +1,23 @@
-def main5(I_xx: list, M: list):
-  
-  return
+def main5(I_xx: list, trapezoids:list, span_and_chord, loads):
+  import numpy as np
+  def Mx(y): 
+    return np.interp(y, span_and_chord[:,1], loads[0][1], 0)
+  max_stress = 450 *10**6 #MPa
+  M_max = 0
+  Failed = False
+  margin_of_safety_list: list = []
+  for i in range(span_and_chord.shape[0]):
+    I_xx = I_xx[i]
+    M = Mx(span_and_chord[i,1])
+    y_max = abs(trapezoids[1,1]*span_and_chord[i,0])
 
+    stress =  (M * y_max)/(I_xx)
+    if stress >= max_stress and not Failed:
+      M = M_max
+      Failed = True
+    
+    margin_of_safety_list.append(max_stress/stress)
+  return margin_of_safety_list, M_max
+
+if __name__ == "__main__":
+  pass

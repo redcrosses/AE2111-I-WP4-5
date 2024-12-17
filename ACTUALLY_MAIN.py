2e70f58c0ac2ce918c4b645ba96f2aed967bcb18
CShiyyy <136488761+CShiyyy@users.noreply.github.com>
2024-12-17 15:43:41 +0100

    object put in main

--- ACTUALLY_MAIN.py
+++ ACTUALLY_MAIN.py
@@ -18,10 +18,11 @@ print(n_negative)
 print("Finding planform loading diagrams...")
 loads_positive, loads_negative, spanwise_position = main1(n_positive, n_negative)
 # print(loads_positive, loads_negative)
-moi_x_list, trapezoid, stringer_positions, span_positions_and_chord = main2((loads_positive, loads_negative), spanwise_position, (n_positive, n_negative), 0.1056927, 0.07702, 0.005, 0.01,  42, 2e-4)
-margin, max = main5(moi_x_list, trapezoid, span_positions_and_chord, (loads_positive, loads_negative),spanwise_position)
-# main4(moi_x_list, trapezoid, stringer_positions, span_positions_and_chord, (loads_positive, loads_negative), spanwise_position)
+design: object = main2((loads_positive, loads_negative), spanwise_position, (n_positive, n_negative), 0.1056927, 0.07702, 0.005, 0.01,  42, 2e-4)
+margin, max = main5(design.moi_x_list, design.trapezoid,  design.chords_along_span, (loads_positive, loads_negative),spanwise_position)
+main4(design.moi_x_list, design.trapezoid, design.stringers, design.chords_along_span, (loads_positive, loads_negative), spanwise_position)
 
+# moi_x_list, trapezoid, stringer_positions, span_positions_and_chord 
 # moi_x_list, trapezoid, stringer_positions, span_positions_and_chord = main2((loads_positive, loads_negative), spanwise_position, (n_positive, n_negative), 0.12079, 0.07702, 0.01, 0.01, 42, 4e-4)
 # margin, max = main5(moi_x_list, trapezoid, span_positions_and_chord, (loads_positive, loads_negative),spanwise_position)
 

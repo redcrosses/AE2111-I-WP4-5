ee7895150f6237f7e4d3a70cc29458ef4955f7ce
LennartH21 <lennart.hubbers@gmail.com>
2024-12-17 16:48:04 +0100

    Update main4.py

--- WP5_1/main4.py
+++ WP5_1/main4.py
@@ -88,7 +88,7 @@ def main4(I_xx, trapezoid, stringers_pos, chord_and_span, loads, spanwise_positi
     def maxshear():
         frontlength = design.chords_along_span[:,0]*design.frontsparlength
         rearlength = design.chords_along_span[:,0]*design.rearsparlength
-        averageshear = V/((frontlength+rearlength)*design.vspar_thickness)
+        averageshear = V(design.chords_along_span)/((frontlength+rearlength)*design.vspar_thickness)
         maxshear = 1.5*averageshear
 
 
